163cf89f4677b2a4d2cc3b1bf4d782a5c2fa5f3a
Talha Ghauri <talhaghauridev@gmail.com>
2024-06-13 07:10:02 -0700

    add clerk authenication and add ui components

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import Events from "@/components/shared/Events";
+import Hero from "@/components/shared/Hero";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Hero />
+      <Events />
+    </>
+  );
+};
+
+export default page;
